2e2b7f340fb744bfd9bcc3fbb81f2998774bfb01
STProgramming <stcorp@outlook.it>
2021-09-22 22:08:00 +0200

    application is now completed

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,36 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 import { Product } from '../services/cart.service';
 import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page implements OnInit{
-
+export class Tab2Page implements OnInit {
   cart: Product[] = [];
-  total: any;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private router: Router
+    ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.cart = this.cartService.getCart();
   }
 
-  decreaseCartItem(product){
+  decreaseCartItem(product) {
     this.cartService.decreaseProduct(product);
   }
 
-  increaseCartItem(product){
+  increaseCartItem(product) {
     this.cartService.addProduct(product);
   }
 
-  removeCartItem(product){
+  removeCartItem(product) {
     this.cartService.removeProduct(product);
   }
 
-  getTotal(){
-    this.total = this.cart.reduce((i , j) => i + j.price * j.amount, 0);
+  getTotal() {
+    return this.cart.reduce((i, j) => i + j.price * j.amount, 0);
+  }
+
+  redirectToHome() {
+    this.router.navigateByUrl('tabs/tab1');
+  }
+
+  async checkOut(){
+    const total = this.getTotal();
+    window.alert('il totale e\' di '+ total +' EUR');
   }
 }
